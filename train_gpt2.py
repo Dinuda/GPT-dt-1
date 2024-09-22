bec5ae1f413011fb2370bc3e8a08208745a51c21
Dinuda Yaggahavita <63149008+Dinuda@users.noreply.github.com>
2024-09-22 16:03:16 +0530

    feat: running on mps

--- train_gpt2.py
+++ train_gpt2.py
@@ -157,6 +157,37 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
         
         return model
-    
+ 
+num_return_sequences = 5
+max_length = 30
+
+   
 model = GPT.from_pretrained('gpt2')
-print("we didnt crash!")
+model.eval() # using not training
+model.to('mps')
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model")
+tokens = torch.tensor(tokens, dtype=torch.long) # torch tensor
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # replicate 5 times
+x = tokens.to('mps')
+
+torch.manual_seed(42)
+torch.mps.manual_seed(42)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        # foward the model to get logits
+        logits = model(x)
+        logits = logits[:, -1, :] #(B, vocab_size)
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+        x = torch.cat((x, xcol), dim=1)
+        
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
