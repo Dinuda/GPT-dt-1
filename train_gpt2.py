ce1e7f7b090bdf98647cf81b5c470f198a963cdf
Dinuda Yaggahavita <63149008+Dinuda@users.noreply.github.com>
2024-09-22 14:53:34 +0530

    feat: add forward() function of GPT2 nn.Module

--- train_gpt2.py
+++ train_gpt2.py
@@ -4,7 +4,7 @@ import math
 import torch.nn as nn
 from torch.nn import functional as F
 
-class CasualSelfAttention(nn.Module):
+class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0 # 384 % 6 == 0 divide embedding dim by number of heads
@@ -24,7 +24,7 @@ class CasualSelfAttention(nn.Module):
         B, T, C = x.size() # batch size, sequence length, embedding size
         
         qkv = self.c_attn(x) # linear projection. Each token emits 3 vectors, query, key, value
-        q, k, v = qkv.chunk(3, dim=-1) # split into query, key, value
+        q, k, v = qkv.split(self.n_embd, dim=2) # split the output into query, key, value
         
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, self.n_head, C // self.n_head) -> (B, self.n_head, T, C // self.n_head)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, T, self.n_head, C // self.n_head) -> (B, self.n_head, T, C // self.n_head)
@@ -40,6 +40,7 @@ class CasualSelfAttention(nn.Module):
         y = y.transpose(1, 2).contiguous().view(B, T, C) # transpose back and reshape - concat   
         # output projection
         y = self.c_proj(y)
+        return y
 
 class MLP(nn.Module):    
     def __init__(self, config):
@@ -58,7 +59,7 @@ class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
-        # self.attn = CasualSelfAttention(config) # TODO:
+        self.attn = CausalSelfAttention(config) 
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
     
@@ -74,7 +75,7 @@ class GPT2Config:
     vocab_size: int = 50257
     n_layer: int = 12
     n_head: int = 12
-    n_embd: int = 786
+    n_embd: int = 768
 
 class GPT(nn.Module):
     
@@ -82,10 +83,80 @@ class GPT(nn.Module):
         super().__init__() 
         self.config = config
         
-        self.transformer - nn.ModuleDict(dict(
+        self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd), # token embeddings
             wpe = nn.Embedding(config.block_size, config.n_embd), # positional embeddings
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]), # hidden layer Nx
             ln_f = nn.LayerNorm(config.n_embd) # layernorm after the last block
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # classifier from embedding to vocab
+        
+    def forward(self, idx):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        return logits
+        
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """ Loads pretrained GPT-2 model weights from hugging face """
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print(f"Loading weights from pretrained model {model_type}")
+        
+        # n_layer, n_head and n_embd are determined by model_type
+        config_args = {
+            'gpt2': dict(n_layer=12, n_head=12, n_embd=768), # 124M
+            'gpt2-medium': dict(n_layer=24, n_head=16, n_embd=1024), # 350M
+            'gpt2-large': dict(n_layer=36, n_head=20, n_embd=1280), # 774M
+            'gpt2-xl': dict(n_layer=48, n_head=25, n_embd=1600) # 1558M
+        }[model_type]
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+        
+        config = GPT2Config(**config_args)
+        model = GPT(config)
+        
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard the attention bias
+
+        # init hugging face transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # discard the attention masked bias
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # discard the attention bias
+        
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        assert len(sd_keys) == len(sd_keys_hf), f"Length mismatch {len(sd_keys)} != {len(sd_keys_hf)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].T) # transpose
+            else:
+                # copy over the other parameters
+                assert sd[k].shape == sd_hf[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        
+        return model
+    
+model = GPT.from_pretrained('gpt2')
+print("we didnt crash!")
